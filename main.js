d13f7dbaab92dc06271341cdb5945048c09f6ee5
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-01-05 01:07:13 +0400

    second section

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
+// first section//
+
 const header = document.querySelector('.header');
 const headerDiv = document.querySelector('.header-div');
 const link = document.querySelectorAll('.link');
+const aboutOneder = document.querySelector('.about-oneder');
+const title = document.querySelector('.title');
+const flex = document.querySelector('.flex');
+
 
 window.onscroll = function(){
      let top = window.scrollY;
-     console.log(top);
+      console.log(top);
      if(top>=100){
        header.classList.add('active');
-       headerDiv.style.color='black';
+       headerDiv.style.color='darkcyan';
       link.forEach((e)=>{
         e.classList.add('active-link');
       })
@@ -17,5 +23,35 @@ window.onscroll = function(){
         link.forEach((e)=>{
             e.classList.remove('active-link');
           })
+         
      }
+      if(top>=0){
+        opacity();
+      }
+     if(top>=250){
+      aboutOneder.classList.add('oneder-active');
+     }
+    
+     if(top>=450){
+        flex.classList.add('flex-active');
+       
+     }
+   
+  setInterval(opacity(), 2000)
+
+
 }
+
+ function opacity(){
+    title.classList.add('title-active');
+ }
+// const title = document.querySelector('.title');
+//  setInterval(opacity(), 1000)
+
+// function opacity(){
+//     title.classList.add('title-active');
+// }
+  
+
+// setTimer(opacity(), 1000);
+
